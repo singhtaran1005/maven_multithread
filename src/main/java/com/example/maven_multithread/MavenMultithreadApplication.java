a6f3ee429314c0a58ed626fb19f27591059572bd
singhtaran1005 <singhh3010@gmail.com>
2021-01-31 12:45:31 +0530

    bsdic_thread_added

--- src/main/java/com/example/maven_multithread/MavenMultithreadApplication.java
+++ src/main/java/com/example/maven_multithread/MavenMultithreadApplication.java
@@ -6,8 +6,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MavenMultithreadApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MavenMultithreadApplication.class, args);
-	}
+    public static void main(String[] args) {
+    //creating an object for class
+        MyThread thread =new MyThread();
+        thread.start();  //now thread will start executing nd a new thread will be created along with a new callstack
+//        thread.run(); // no new thread
+
+    }
+
+    public static class MyThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("in thread" + currentThread().getName());
+        }
+    }
 
 }
+//daemon thread - basically a bg thread
+//priority - which thread should be executed when (scheduling_)
